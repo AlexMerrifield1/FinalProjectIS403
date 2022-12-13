464ac59087ff249d633e6528d5922b61a8f18679
AlexMerrifield1 <alex.merrifield13@gmail.com>
2022-12-12 20:59:24 -0700

    Monday Night

--- aptmanagement/views.py
+++ aptmanagement/views.py
@@ -28,20 +28,35 @@ def TenantInfoPage(request) :
 
 def AddTennant(request) :
     if request.method == 'POST':
-        apartment = Apartments()
-        apartment.house = request.POST['house']
-        apartment.rent = request.POST['rent']
-        apartment.save()
 
+        #Create new Tennant
         tennant = Tennant()
         tennant.first_name = request.POST['first_name']
         tennant.last_name = request.POST['last_name']
         tennant.rent_start = request.POST['rent_start_date']  
         tennant.rent_end = request.POST['rent_end_date']
         tennant.phone = request.POST['phone'] 
+
+        #Use existing Apartments or create new Apartment
+        data = Apartments.objects.all()
+        inDictionary = False
+        for apartments in data :
+            if apartments.house != request.POST['house'] :
+                continue
+            elif apartments.house == request.POST['house'] :
+                inDictionary = True
+                apartment = Apartments.objects.get(house = request.POST['house'])
+                
+        #Create new Apartment if not already created
+        if inDictionary == False :
+            apartment = Apartments()
+            apartment.house = request.POST['house']
+            apartment.rent = request.POST['rent']
+            apartment.save()
+
+
         tennant.appartment = apartment
         tennant.save()
 
-        
 
     return TenantInfoPage(request)
